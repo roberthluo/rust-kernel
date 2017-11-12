b4965992e9181fd1da05d5361b0d14aea22fc395
robert <robert.otherone@gmail.com>
2017-11-12 11:37:49 -0500

    Added comments to boot code

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -1,8 +1,14 @@
+;entry point of kernel
+global start
 
-bal start
 
+;default section for executable code
 section .text
+
+;when the GRUB starts the kernel, CPU uses protected mode, which is 16 bits 
+;following is for 32-bit instructions
 bits 32
 start:
+    ;prints 'OK' to screen
     mov dword[0xb8000], 0x2f4b2f4f
     hlt
