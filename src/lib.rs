6e0b3bc1696a8e12426029c7ee3b0429a1df487b
robert <robert.otherone@gmail.com>
2017-11-25 15:59:14 -0500

    Create module for text buffer and linked lib.rs to it

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-
+#![feature(const_fn)]
+#![feature(unique)]
 //#! defines attribute of current module
 //attributes are metadata like lints, compiler features, links to foreign library
 //feature allows feature-gated attributes (not stable) compilers
@@ -10,8 +11,25 @@
 extern crate rlibc;
 //Disables automatic name mangling
 //Want to call rust_main from assembly
+#[macro_use]
+mod vga_buffer;
 #[no_mangle]
-pub extern fn rust_main() {}
+pub extern fn rust_main() {
+    // ATTENTION: we have a very small stack and no guard page
 
+    let hello = b"Hello World!";
+    let color_byte = 0x1f; // white foreground, blue background
+
+    let mut hello_colored = [color_byte; 24];
+    for (i, char_byte) in hello.into_iter().enumerate() {
+        hello_colored[i*2] = *char_byte;
+    }
+
+    // write `Hello World!` to the center of the VGA text buffer
+    let buffer_ptr = (0xb8000 + 1988) as *mut _;
+    unsafe { *buffer_ptr = hello_colored };
+
+    loop{}
+}
 #[lang = "eh_personality"] extern fn eh_personality() {}
 #[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {loop{}}
